52bb979ec7823f6d064b8824c33e31f3701be911
Rajashree Gaykar <68843570+Rajashre@users.noreply.github.com>
2020-10-27 11:27:14 +0530

    Add files via upload

---
+++ Assg2_1/assg2_1.js
@@ -0,0 +1,9 @@
+function Maximum() {
+    var arr = [23, 89, 6, 29, 56, 45, 77, 32];
+    var largest = 0;
+    for (i = 0; i <= largest; i++) {
+        if (array > largest)
+            var lrgest = arr[i];
+    }
+}
+console.log(largest);
---
+++ Assg2_1/assg2_1.ts
@@ -0,0 +1,13 @@
+
+    var array=[23,89,6,29,56,45,77,32];
+    var largest=0;
+
+    for(i=0;i<=largest;i++)
+    {
+        if (array > largest)
+        var largest=array[i];
+    }
+}
+console.log(largest);
+
+   
